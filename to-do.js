2aedc769ab289a6e49cc62f3fae4fea9f506a723
manas gupta <manasgupta0110@gmail.com>
2023-02-26 00:18:37 +0530

    Array to-do

--- to-do.js
+++ to-do.js
@@ -1,47 +1,44 @@
-const enterBtn = document.getElementById('enterBtn');
-const inputField = document.getElementById('inputField');
-const orderedList = document.getElementById('orderedList');
-const clearBtn = document.getElementById('clearBtn');
 
+const input = document.getElementById('input');
+const itemsDisplayed = document.getElementById('itemsDisplayed');
+var itemsAdded = [];
 
-function addInputToList(){
-    if(inputField.value === ''){
-        alert("enter a valid item");
-        return;}
-    
-let listed = document.createElement('li');
-listed.innerText = inputField.value ;
-orderedList.appendChild(listed);
-inputField.value = '';
-let a = document.createElement('a');
-a.href = "javascript:void(0)";
-a.innerText = " X" ;
-listed.appendChild(a);
-a.addEventListener('click', function(){
-    orderedList.removeChild(listed);  
-})
+function newItemsAdded(){
+    itemsAdded.push(input.value)
+    input.value = ''
+    console.log(itemsAdded, '@@@@')
+    itemsAdded.map(item => {itemsDisplayed.appendChild(document.createTextNode(item))})
 
-listed.addEventListener('click', ()=> {
-   
-    let replacedItem =  prompt("enter the replaced item");
-    listed.innerText = replacedItem;
-    listed.appendChild(a)
-
-})
 
 }
 
+function itemRemoved(){
+    itemsAdded.splice(input.value - 1, 1)
+    input.value = ''
+    JSON.stringify(itemsAdded)
+    console.log(itemsAdded, '@@@@')
+}
 
-enterBtn.addEventListener('click', addInputToList);
+function itemEdited(){
+    itemsAdded.splice(input.value -1, 1, prompt('enter the item you want to add'))
+    input.value = ''
+    JSON.stringify(itemsAdded)
+console.log(itemsAdded, '@@@@')
+}
 
-window.addEventListener('keypress', (e) =>{
-    if(e.charCode === 13){
-        addInputToList();
-    }
-})
+JSON.stringify(itemsAdded)
+console.log(itemsAdded, '@@@@')
 
-clearBtn.addEventListener('click',() =>{
-    // orderedList.replaceChildren();
-    orderedList.innerHTML = '';
-})
-
+Array.prototype.listeners = {};
+Array.prototype.addListener = function(eventName, callback) {
+  if (!this.listeners[eventName]) {
+    // Create a new array for new events
+    // idea of an array is we can invoke all callbacks
+    this.listeners[eventName] = [];
+  }
+  this.listeners[eventName].push(callback);
+};
+itemsAdded.addListener('change', () => console.log('Hello'))
+itemsAdded.addListener('change', () => console.log('Hello'))
+itemsAdded.addListener('add', () => console.log('World'))
+console.log(itemsAdded.listeners)
