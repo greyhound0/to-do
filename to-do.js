5dfdc46c86aa8ffa4004509bf71bc442bfc3b8e0
manas gupta <manasgupta0110@gmail.com>
2023-02-26 02:05:08 +0530

    prototype approach

--- to-do.js
+++ to-do.js
@@ -1,44 +1,59 @@
 
+Array.prototype.listeners = {};
+Array.prototype.addListener = function(eventName, callback) {
+    if (!this.listeners[eventName]) {
+   
+    this.listeners[eventName] = [];
+  }
+  this.listeners[eventName].push(callback);
+};
+Array.prototype.pushWithEvent = function( ...args) {
+  const size = this.length;
+
+  for (let index = 0; index < args.length; index++) {
+    this[size + index] = args[index];
+  }
+
+  this.triggerEvent('change', args);
+};
+
+Array.prototype.spliceWithEvent = function(start, deleteCount = 1, ...args) {
+    this.splice(start, deleteCount, ...args)
+
+    this.triggerEvent('change', this)
+}
+
+Array.prototype.triggerEvent = function(eventName, elements) {
+  if (this.listeners[eventName] && this.listeners[eventName].length) {
+    this.listeners[eventName].forEach(callback =>
+      callback(eventName, elements, this)
+    );
+  }
+};
+
 const input = document.getElementById('input');
 const itemsDisplayed = document.getElementById('itemsDisplayed');
 var itemsAdded = [];
 
 function newItemsAdded(){
-    itemsAdded.push(input.value)
+    itemsAdded.pushWithEvent(input.value)
     input.value = ''
-    console.log(itemsAdded, '@@@@')
-    itemsAdded.map(item => {itemsDisplayed.appendChild(document.createTextNode(item))})
+}
 
+function itemRemoved(index){
+    itemsAdded.spliceWithEvent(index, 1)
+}
+
+function itemEdited(index){
+    itemsAdded.spliceWithEvent(input.value -1, 1, prompt('enter the item you want to add'))
 
 }
 
-function itemRemoved(){
-    itemsAdded.splice(input.value - 1, 1)
-    input.value = ''
-    JSON.stringify(itemsAdded)
-    console.log(itemsAdded, '@@@@')
-}
 
-function itemEdited(){
-    itemsAdded.splice(input.value -1, 1, prompt('enter the item you want to add'))
-    input.value = ''
-    JSON.stringify(itemsAdded)
-console.log(itemsAdded, '@@@@')
-}
+itemsAdded.addListener('change',(name,e, array)=> {
+    itemsDisplayed.innerHTML = ''
+    array.map((item, i) => itemsDisplayed.innerHTML += `<li>${item} <button onclick="itemEdited(${i})">edit</button> <span onclick="itemRemoved(${i})">X</span></li>`)
+});
 
-JSON.stringify(itemsAdded)
-console.log(itemsAdded, '@@@@')
 
-Array.prototype.listeners = {};
-Array.prototype.addListener = function(eventName, callback) {
-  if (!this.listeners[eventName]) {
-    // Create a new array for new events
-    // idea of an array is we can invoke all callbacks
-    this.listeners[eventName] = [];
-  }
-  this.listeners[eventName].push(callback);
-};
-itemsAdded.addListener('change', () => console.log('Hello'))
-itemsAdded.addListener('change', () => console.log('Hello'))
-itemsAdded.addListener('add', () => console.log('World'))
-console.log(itemsAdded.listeners)
+
