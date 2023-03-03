2bd4e459d4af6bfecaabe5260c9176fb5e715143
manas gupta <manasgupta0110@gmail.com>
2023-03-03 20:04:25 +0530

    new edit

--- to-do.js
+++ to-do.js
@@ -13,15 +13,21 @@ let listed = document.createElement('li');
 listed.innerText = inputField.value ;
 orderedList.appendChild(listed);
 inputField.value = '';
+let editButton = document.createElement('button');
+editButton.className = "editButton"
+editButton.innerText = "Edit"
+listed.appendChild(editButton);
 let a = document.createElement('a');
 a.href = "javascript:void(0)";
 a.innerText = " X" ;
+a.style.color = "rgb(209,8,8)"
 listed.appendChild(a);
+
 a.addEventListener('click', function(){
     orderedList.removeChild(listed);  
 })
 
-listed.addEventListener('click', ()=> {
+editButton.addEventListener('click', ()=> {
    
     let replacedItem =  prompt("enter the replaced item");
     listed.innerText = replacedItem;
